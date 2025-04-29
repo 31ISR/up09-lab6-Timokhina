941135dd16ffb679d9d16073e2749ed2eb72db7f
SofyaPalna <sosyenka@gmail.com>
2025-04-29 14:16:05 +0000

    feat(app/Http/Controllers/GoodbyeController.php):add new controller; refactor(routes/web.php):added a controller to the app; refactor(resources/views/goodbye.blade.php):changed the template for the controller view;

--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\GoodbyeController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::get('/goodbye', [GoodbyeController::class, 'goodbye'])->name('goodbye');
