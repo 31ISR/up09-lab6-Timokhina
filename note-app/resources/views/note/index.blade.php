d4c09d1b229795c9b8f5cda7eb0d125582c84afc
Timokhina Sofya <sosyenka@gmail.com>
2025-09-25 20:12:22 +0600

    refactor(NoteController):change method index and working with content in show and edit; refactor(index.blade):change the structure to display all notes from DB;

--- note-app/resources/views/note/index.blade.php
+++ note-app/resources/views/note/index.blade.php
@@ -1,28 +1,23 @@
 <x-layout>
-    <div >
-        <a href="{{ route('note.create') }}" >
-            New Note
-        </a>
-        <div >
-            @foreach ($notes as $note)
-                <div >
-                    <div >
-                        {{ Str::words($note->note, 30) }}
-                    </div>
-                    <div >
-                        <a href="{{ route('note.show', $note) }}" >View</a>
-                        <a href="{{ route('note.edit', $note) }}" >Edit</a>
-                        <form action="{{ route('note.destroy', $note) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button >Delete</button>
-                        </form>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+    <div class="main-container">
+        <a href="{{ route('note.create') }}" class="new-note-btn">New Note</a>
 
-        <div >
+        @foreach ($notes as $note)
+            <div class="view-screen">
+                <div class="note-content">{{ Str::words($note->note, 30) }}</div>
+                <div class="action-group">
+                    <a href="{{ route('note.show', $note) }}" class="action-btn view-btn">View</a>
+                    <a href="{{ route('note.edit', $note) }}" class="action-btn edit-btn">Edit</a>
+                    <form action="{{ route('note.destroy', $note) }}" method="POST" style="display: inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="action-btn delete-btn">Delete</button>
+                    </form>
+                </div>
+            </div>
+        @endforeach
+
+        <div class="pagination">
             {{ $notes->links() }}
         </div>
     </div>
--- note-app/resources/views/note/show.blade.php
+++ note-app/resources/views/note/show.blade.php
@@ -1,19 +1,15 @@
 <x-layout>
-    <div>
-        <div>
+    <div class="main-container">
+        <div class="view-screen">
             <h1>Note: {{ $note->created_at }}</h1>
-            <div>
-                <a href="{{ route('note.edit', $note) }}">Edit</a>
-                <form action="{{ route('note.destroy', $note) }}" method="POST">
+            <div class="note-content">{{ $note->note }}</div>
+            <div class="action-group">
+                <a href="{{ route('note.edit', $note) }}" class="action-btn edit-btn">Edit</a>
+                <form action="{{ route('note.destroy', $note) }}" method="POST" style="display: inline;">
                     @csrf
                     @method('DELETE')
-                    <button>Delete</button>
+                    <button type="submit" class="action-btn delete-btn">Delete</button>
                 </form>
-            </div>
-        </div>
-        <div>
-            <div>
-                {{ $note->note }}
             </div>
         </div>
     </div>
